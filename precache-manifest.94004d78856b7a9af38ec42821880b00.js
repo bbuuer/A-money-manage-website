3464de49bd2dd8fd465f0d6a657469a5945ca02a
Othmer <wer123da@qq.com>
2021-11-02 22:18:41 +0800

    deploy

---
+++ precache-manifest.94004d78856b7a9af38ec42821880b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1eec098b8d264b82d5d",
+    "url": "/A-money-manage-website/css/app.59e9abad.css"
+  },
+  {
+    "revision": "85e738b64134f00d54cc1d4e0102f187",
+    "url": "/A-money-manage-website/index.html"
+  },
+  {
+    "revision": "b1eec098b8d264b82d5d",
+    "url": "/A-money-manage-website/js/app.81cebb34.js"
+  },
+  {
+    "revision": "151354bbb8e35aa19b3c",
+    "url": "/A-money-manage-website/js/chunk-vendors.9bf56c29.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/A-money-manage-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/A-money-manage-website/robots.txt"
+  }
+]);
